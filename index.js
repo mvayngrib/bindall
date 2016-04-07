186ad6027fb826d89372d95a80c099f9cc62e2e0
Mark Vayngrib <mark@tradle.io>
2016-04-06 20:20:26 -0400

    support 1 arg version

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 
 module.exports = function bindAll (obj) {
   var i, length = arguments.length, key
+  if (length === 1) {
+    var args = Object.keys(obj).filter(function (key) {
+      return typeof obj[key] === 'function'
+    })
+
+    args.unshift(obj)
+    return bindAll.apply(null, args)
+  }
+
   if (length <= 1) throw new Error('bindAll must be passed function names')
 
   for (i = 1; i < length; i++) {
--- test/index.js
+++ test/index.js
@@ -2,7 +2,7 @@ var test = require('tape')
 var bindAll = require('../')
 
 test('bindAll', function(t) {
-  t.plan(8)
+  t.plan(7)
 
   var curly = {name: 'curly'}, moe = {
     name: 'moe',
@@ -23,7 +23,6 @@ test('bindAll', function(t) {
     sayLast: function() { return this.sayHi([].slice.call(arguments).pop()) }
   }
 
-  t.throws(function() { bindAll(moe) }, Error, 'throws an error for bindAll with no functions named')
   t.throws(function() { bindAll(moe, 'sayBye') }, TypeError, 'throws an error for bindAll if the given key is undefined')
   t.throws(function() { bindAll(moe, 'name') }, TypeError, 'throws an error for bindAll if the given key is not a function')
 
@@ -37,4 +36,23 @@ test('bindAll', function(t) {
   bindAll(moe, ['getName'])
   var getName = moe.getName
   t.equal(getName(), 'name: moe', 'flattens arguments into a single list')
+})
+
+test('1 arg', function (t) {
+  t.plan(2)
+  var obj = {
+    a: 1,
+    b: function () {
+      t.equal(this, obj)
+    },
+    c: function () {
+      t.equal(this, obj)
+    }
+  }
+
+  bindAll(obj)
+  ;['b', 'c'].forEach(function (name) {
+    var fn = obj[name]
+    fn()
+  })
 })
